fc9277f71856b5011b71062383cec094fb4f8177
Richard Otter <rjo7rjo7@gmail.com>
2021-06-30 10:26:09 -0700

    Update TestExternalFiles.py
    Now uses ini file for configuration.

--- TestExternalFiles.py
+++ TestExternalFiles.py
@@ -1,14 +1,31 @@
 import sqlite3
-import sys
 import os
-from pathlib import Path
+from pathlib  import Path
+from datetime import datetime
+import configparser
 
-
-## Tested with RootsMagic v7.6.5
+## Tested with RootsMagic v7.6.5  (not tested with RM 8)
 ##             Python for Windows v3.9.0
 ##             unifuzz64.dll (ver not set, MD5=06a1f485b0fae62caa80850a8c7fd7c2)
 ##                   from:   https://sqlitetoolsforrootsmagic.com/rmnocase-faking-it-in-sqlite-expert-command-line-shell-et-al/
 
+## Configuration ini file:    RM-Python-config.ini
+## exmaple ini file:
+#        
+#        [File Paths]
+#        DB_PATH      = C:\MyDatabae.rmgc
+#        REPORT_PATH  = C:\Exhibit File report.txt  
+#        RMNOCASE_PATH = C:\RMNOCASE\unifuzz64.dll
+#        
+##
+
+# TODO
+# implement "NotReferencedFile function
+# better error handling opening database
+# check database schema version
+
+
+# ================================================================
 def create_connection(db_file):
     conn = None
     try:
@@ -19,8 +36,8 @@ def create_connection(db_file):
     return conn
 
 
-
-def ListFolders(conn):
+# ================================================================
+def ListFolders(conn, reportF):
 
     SqlStmt="""\
       SELECT  DISTINCT MediaPath
@@ -31,16 +48,17 @@ def ListFolders(conn):
     cur.execute(SqlStmt)
 
     rows = cur.fetchall()
-    print ("MultimediaTable has each of these folders mentioned at least once:\n\n")
+    reportF.write ("Folder List:\n\n")
     for row in rows:
-        print(row[0])
-    print (len(rows), "rows returned \n\n")
+        reportF.write(row[0] + "\n")
+    reportF.write (str(len(rows)) + "  rows returned \n\n")
 
     return rows
 
 
 
-def CheckAllFiles(conn):
+# ================================================================
+def CheckAllFiles(conn, reportF):
 
     SqlStmt="""\
       SELECT  MediaPath, MediaFile
@@ -49,66 +67,95 @@ def CheckAllFiles(conn):
 """
     cur = conn.cursor()
     cur.execute(SqlStmt)
+    
+    reportF.write ("File not found warnings:\n\n")
 
     for row in cur:
-      checkPath (row)
+      checkPath (row, reportF)
 
 
 
-def checkPath( row ):
+# ================================================================
+def checkPath( row, reportF ):
     dirPath=Path(row[0])
     filePath=Path(row[0] + row[1])
 
-    #print (dirPath)
-    #print (filePath)
-
     if not dirPath.exists(): 
-       print ("Directory path not found:", "\"" + str(dirPath) + "\"")
+       reportF.write ("Directory path not found:" + "\"" + str(dirPath) + "\"" + "\n")
        return
     else:
         if filePath.exists():
             if not filePath.is_file():
-                print ("File path is not a file:", "\"" + str(filePath) + "\"")
+                reportF.write ("File path is not a file:" + "\"" + str(filePath) + "\"" + "\n")
                 return
         else:
-            print ("File path not found:", "\"" + str(filePath) + "\"")
+            reportF.write ("File path not found:" + "\"" + str(filePath) + "\"" + "\n")
             return
     #print ("Path OK:", "\"" + str(filePath) + "\"")
     return
 
 
+# ================================================================
+def NonReferencedFiles():
+     # TODO
+     return
 
+
+# ================================================================
+def TS():
+     # datetime object containing current date and time
+     now = datetime.now()
+     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+     return dt_string
+     
+# ================================================================
 def main():
-    databasePath=os.getenv("DB_PATH")
-    if databasePath == None:
-    # test database
-        databasePath = r"C:\Users\rotter\Documents\Genealogy\Genealogy SW\RootsMagic\SQL access\test DB\test copy Otter-Saito.rmgc"
-    # production
-        #databasePath = r"C:\Users\rotter\Documents\Genealogy\GeneDB\Otter-Saito.rmgc"
-
-    RMNOCASE_extPath=os.getenv("RMNOCASEPATH")
-    if RMNOCASE_extPath == None:
-        RMNOCASE_extPath=r"C:\Users\rotter\Documents\Genealogy\Genealogy SW\RootsMagic\SQL access\RMNOCASE\unifuzz64.dll"
-
-
-    # create a database connection
+    # Configuration
+    IniFile="RM-Python-config.ini"
     
-    conn = create_connection(databasePath)
-    conn.enable_load_extension(True)
-    conn.load_extension(RMNOCASE_extPath)
+    # ini file must be in current directory
+    if not os.path.exists(IniFile):
+        print("The ini configuration file, " + IniFile + " must be in the current directory." )
+        return
 
-    print("Database in use:", databasePath)
-    input("Press the <Enter> key to continue...")
+    config = configparser.ConfigParser()
+    config.read('RM-Python-config.ini')
+    
+    # Read file paths from ini file
+    report_Path = config['File Paths']['REPORT_PATH']
+    database_Path = config['File Paths']['DB_PATH']
+    RMNOCASE_Path = config['File Paths']['RMNOCASE_PATH']
 
-    with conn:
-       ListFolders(conn)
-       input("Press the <Enter> key to continue...")
-       CheckAllFiles(conn)
+    if not os.path.exists(database_Path):
+        print('Database path not found')
+        return
+    
+   # Process the database
+   
+    with create_connection(database_Path) as conn:
+      conn.enable_load_extension(True)
+      conn.load_extension(RMNOCASE_Path)
+    
+      with open( report_Path, "w") as reportF:
+      
+        reportF.write ("report generated at = " + TS() + "\n")	
+        reportF.write ("Database processed  = " + database_Path + "\n\n\n")
+        
+        CheckAllFiles(conn, reportF)
+        
+        reportF.write ("\n\n\n")
+        
+        ListFolders(conn, reportF)
+        
+        reportF.write ("\n\n\n")
 
-   # testData=[ r"C:\Users\rotter\source\Python\TestExternalFiles\ "[:-1] , "test.py"]
-   # checkPath(testData)
-
-
+        NonReferencedFiles()
+        
+        reportF.write ("\n\n\n")
+       
+        reportF.write ("End of report \n")
+  
+       
 
 if __name__ == '__main__':
     main()
