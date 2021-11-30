b961d63e0e29502096e7c29b6c9f0c39af2eebae
Richard Otter <rjo7rjo7@gmail.com>
2021-11-30 15:14:27 -0800

    Moves citation from old to new src

--- RM -LumpSources/LumpSources.py
+++ RM -LumpSources/LumpSources.py
@@ -6,24 +6,19 @@ from datetime import datetime
 import configparser
 import xml.etree.ElementTree as ET
 
-##  This script only reads RootsMagic database files and will not harm it.
-## However, until trust is established, make a backup before use.
+## WARNING make a known-good backup of the rmtree file before use.
 
 ##  Requirements: (see ReadMe.txt for details)
-##   RootsMagic v7 or v8.0 database file
+##   RootsMagic v8 database file
 ##   RM-Python-config.ini  ( Configuration ini file to set options and parameters)
 ##   unifuzz64.dll
-##   Python v3.9 or greater
+##   Python v3.10 or greater
 
 # TODO
 # better error handling when opening database
 
 
-# ================================================================
-#  Global Variable
-#  paths may be relative to RootsMagic's "Media folder", or database file folder, or home dir
-G_MediaDirectoryPath = ""
-G_DbFileFolderPath = ""
+
 
 G_Divider = "==========================================================="
 G_QT = "\""
@@ -41,176 +36,83 @@ def create_DBconnection(db_file):
 
 
 # ================================================================
-def ListFoldersFeature(conn, reportF):
-    SqlStmt="""\
-      SELECT  DISTINCT MediaPath
-      FROM MultimediaTable
-        ORDER BY MediaPath
-"""
+def GetListOfRows ( conn, SqlStmt):
+    # SqlStmt should return a set of single values
     cur = conn.cursor()
     cur.execute(SqlStmt)
 
-    rows = cur.fetchall()
-    reportF.write (G_Divider + "\nStart of \"Referenced Folders\" listing\n\n")
-
-    for row in rows:
-      reportF.write(row[0] + "\n")
-    reportF.write ("\n" + str(len(rows)) + "  folders referenced in RootsMagic file \n")
-
-    reportF.write ("End of \"Referenced Folders\" listing\n\n")
-
-    return rows
+    result = []
+    for t in cur:
+      for x in t:
+        result.append(x)
+    return result
 
 
 # ================================================================
-def GetDBFileList(conn):
-    SqlStmt="""\
-      SELECT  MediaPath, MediaFile
-      FROM MultimediaTable
-        ORDER BY MediaPath, MediaFile
-"""
+def runSQL ( conn, SqlStmt):
+    # SqlStmt should return nothing
     cur = conn.cursor()
     cur.execute(SqlStmt)
-    return cur
+    return
 
-
-# ================================================================
-def ExpandDirPath(in_path):
-  # deal with relative paths in RootsMagic 8 databases
-  # RM7 path are always absolute and will never be changed here
-
-  global G_MediaDirectoryPath
-  path = str(in_path)
-  # input parameter path should always be of type str, output will be Path
-  # note when using Path / operator, second operand should not be absolute
-
-  if path[0] == "~" :
-     absolutePath = Path(os.path.expanduser(path))
-
-  elif path[0] == "?":
-    if G_MediaDirectoryPath == "":
-       G_MediaDirectoryPath = GetMediaDirectory()
-    if len(path) == 1 : absolutePath = Path(G_MediaDirectoryPath)
-    else: absolutePath = Path(G_MediaDirectoryPath) / path[2:]
-
-  elif path[0] == "*":
-    if len(path) == 1 : absolutePath = Path(G_DbFileFolderPath)
-    else: absolutePath = Path(G_DbFileFolderPath) / path[2:]
-
-  else:
-    absolutePath = Path(path)
-
-  return absolutePath
 
 
 # ================================================================
-def GetMediaDirectory():
-#  File location set by RootsMagic installer
-  RM_Config_FilePath = r"~\AppData\Roaming\RootsMagic\Version 8\RootsMagicUser.xml"
-
-#  if file not found, RM8 not installed, Media folder path will never be used
-  myPath=Path(os.path.expanduser(RM_Config_FilePath))
-  if not myPath.exists():
-    path = "RM8 not installed"
-  else:
-    root = ET.parse(myPath)
-    MediaFolderPathEle = root.find( "./Folders/Media")
-    path = MediaFolderPathEle.text
-  return path
-
+def GetOldSrc ( conn, SourceID):
+    return
 
 # ================================================================
-def ListMissingFilesFeature( conn, reportF ):
-  cur= GetDBFileList(conn)
+def MoveCitation ( conn, oldSourceID, newSourceID):
+    # get citations for oldSourceID
+    SqlStmt = """
+    SELECT CitationID
+      FROM CitationTable
+      WHERE SourceID = ?
+      """
 
-  reportF.write (G_Divider + "\nStart of \"Files Not Found\" listing\n\n")
-  foundSomeMissingFiles=False
-  for row in cur:
-    dirPath = ExpandDirPath(row[0])
-    filePath = dirPath / row[1]
-    if not dirPath.exists(): 
-       foundSomeMissingFiles=True
-       reportF.write ("Directory path not found:" + G_QT + str(dirPath) + G_QT + " for file: " + G_QT + row[1] + G_QT + "\n")
-    else:
-        if filePath.exists():
-            if not filePath.is_file():
-                foundSomeMissingFiles=True
-                reportF.write ("File path is not a file: " + G_QT + str(filePath) + G_QT + "\n")
-        else:
-            foundSomeMissingFiles=True
-            reportF.write ("File path not found: " + G_QT + str(filePath) + G_QT + "\n")
+    cur = conn.cursor()
+    cur.execute(SqlStmt, (oldSourceID,))
+    citationsIDs = cur.fetchall()
 
-  if foundSomeMissingFiles == False:
-     reportF.write ("    No files were found missing.\n")
-  reportF.write ("\nEnd of \"Files Not Found\" listing\n\n")
-  return
+    # test number of citatons found
+    if len(citationsIDs) == 0 :
+        return
+    if len(citationsIDs) > 1 :
+        print( "an old source has more than one citation /n", oldSourceID )
+        return
+
+    citationIDToMove = citationsIDs[0][0]
 
 
-# ================================================================
-def FolderContentsMinusIgnored(dirPath, config):
-  ignoredFolderNames = config.get('Ignored Objects', 'folders').split('\n')
-  ignoredFileNames   = config.get('Ignored Objects', 'filenames').split('\n')
 
-  mediaFileList = []
-  for (dirname, dirnames, filenames) in os.walk(dirPath, topdown=True):
 
-    for igFldrName in ignoredFolderNames:
-      if igFldrName in dirnames:
-       dirnames.remove(igFldrName)
+    SqlStmt = """
+    UPDATE CitationTable
+      SET SourceID = ?
+      WHERE CitationID = ?
+      """
 
-    for igFileName in ignoredFileNames:
-      if igFileName in filenames:
-        filenames.remove(igFileName)
+    cur = conn.cursor()
+    cur.execute(SqlStmt, (newSourceID, citationIDToMove))
 
-    for filename in filenames:
-        mediaFileList.append(str(os.path.join(dirname, filename)))
+# don't delete untill data copied to new citation
 
-  return mediaFileList
+    SqlStmt = """
+    DELETE from SourceTable
+      WHERE SourceID = ?
+      """
+    cur.execute(SqlStmt, (oldSourceID,))
 
+    conn.commit()
+
+    return
 
 # ================================================================
-def ListUnReferencedFilesFeature(config, conn, reportF):
-  ExtFilesFolderPath = Path(config['File Paths']['SEARCH_ROOT_FLDR_PATH'])
-  if not ExtFilesFolderPath.exists(): 
-    reportF.write ("ERROR: Directory path not found:" + "\"" + str(ExtFilesFolderPath) + "\"" + "\n")
-    sys.exit()
-  if not ExtFilesFolderPath.is_dir():
-    reportF.write ("ERROR: Path is not a directory:" + "\"" + str(ExtFilesFolderPath) + "\"" + "\n")
-    sys.exit()
-  cur= GetDBFileList(conn)
+def Convert (conn, oldSrc, NewSourceID):
+    MoveCitation ( conn, oldSrc, NewSourceID)
 
-  dbFileList=[]
-  for row in cur:
-    dirPath=ExpandDirPath(row[0])
-    filePath=os.path.join(dirPath, row[1])
-    dbFileList.append(filePath)
+    return
 
-  mediaFileList = FolderContentsMinusIgnored(ExtFilesFolderPath, config)
-
-  unRefFiles = list(set(mediaFileList).difference(dbFileList))
-  unRefFiles.sort()
-
-  reportF.write(G_Divider + "\nStart \"Unreferenced Files\" listing\n\n")
-  reportF.write("Folder to inventory: " + str(ExtFilesFolderPath) + "\n")
-  reportF.write("External files folder contains " + str(len(mediaFileList)) 
-       + " files (not counting ignored items)\n")
-  reportF.write("Database contains " + str(len(dbFileList)) + " file links\n")
-  reportF.write( "Number of files in External files folder not referenced by the database: "
-       + str(len(unRefFiles))  + "\n\n\n")
-
-  # don't print full path from root folder
-  cutoff = len(str(ExtFilesFolderPath))
-
-  for i in range(len(unRefFiles)):
-    reportF.write("." + str(unRefFiles[i])[cutoff:] + "\n")
-
-  reportF.write("\nEnd \"Unreferenced Files\" listing\n\n")
-  return
-
-
-# ================================================================
-def CheckForTrue( inputString):
-     return inputString.lower()  in ['on', 'true', '1', 't', 'y', 'yes']
 
 
 # ================================================================
@@ -223,61 +125,56 @@ def TimeStamp():
 
 # ================================================================
 def main():
-  global G_DbFileFolderPath
 
-  # Configuration file
-  IniFile="RM-Python-config.ini"
-  
-  # ini file must be in "current directory" and encoded as UTF-8 if non-ASCII chars present (no BOM)
-  if not os.path.exists(IniFile):
-      print("ERROR: The ini configuration file, " + IniFile + " must be in the current directory." )
-      return
+    # Configuration file
+    #  IniFile="C:/Users/rotter/Development/Genealogy/Genealogy-scripts/RM -LumpSources/" + "RM-Python-config.ini"
+    IniFile="RM-Python-config.ini"
 
-  config = configparser.ConfigParser()
-  config.read('RM-Python-config.ini', 'UTF-8')
+    # ini file must be in "current directory" and encoded as UTF-8 if non-ASCII chars present (no BOM)
+    if not os.path.exists(IniFile):
+        print("ERROR: The ini configuration file, " + IniFile + " must be in the current directory." )
+        return
 
-  # Read file paths from ini file
-  report_Path   = config['File Paths']['REPORT_PATH']
-  database_Path = config['File Paths']['DB_PATH']
-  RMNOCASE_Path = config['File Paths']['RMNOCASE_PATH']
+    config = configparser.ConfigParser()
+    config.read(IniFile, 'UTF-8')
 
-  if not os.path.exists(database_Path):
-      print('Database path not found')
-      return
+    # Read file paths from ini file
+    database_Path = config['File Paths']['DB_PATH']
+    RMNOCASE_Path = config['File Paths']['RMNOCASE_PATH']
 
-  FileModificationTime = time.ctime( os.path.getmtime(database_Path))
+    if not os.path.exists(database_Path):
+        print('Database path not found')
+        return
 
-  G_DbFileFolderPath = Path(database_Path).parent
 
-  # Read processing options from ini file
-  ListFilesNotFound        = config['Options']['CHECK_FILES']
-  ListAllReferencedFolders = config['Options']['FOLDER_LIST']
-  ListUnReferencedFiles    = config['Options']['UNREF_FILES']
+    # Process the database
+    with create_DBconnection(database_Path) as conn:
+      conn.enable_load_extension(True)
+      conn.load_extension(RMNOCASE_Path)
 
-  # Process the database
-  with create_DBconnection(database_Path) as conn:
-    conn.enable_load_extension(True)
-    conn.load_extension(RMNOCASE_Path)
+    # specific to each run
+    # specify which source is to get the converted citations
+    NewSourceID = 5502
 
-    with open( report_Path,  mode='w', encoding='utf-8-sig') as reportF:
-      reportF.write ("Report generated at = " + TimeStamp() + "\n")  
-      reportF.write ("Database processed  = " + database_Path + "\n")
-      reportF.write ("Database last changed on = " + FileModificationTime + "\n\n")
+    # specific to each run
+    # List the sources to be lumped
+    SqlStmt="""\
+SELECT SourceID
+  FROM SourceTable
+  WHERE  Name LIKE 'SSDI%'
+         AND TemplateID=10008
+"""
 
-      if CheckForTrue(ListFilesNotFound):
-         ListMissingFilesFeature(conn, reportF)
 
-      if CheckForTrue(ListUnReferencedFiles):
-  #       reportF.write ("\n\n")
-         ListUnReferencedFilesFeature(config, conn, reportF)
+    SourcesToLump = GetListOfRows( conn, SqlStmt)
 
-      if CheckForTrue(ListAllReferencedFolders):
- #        reportF.write ("\n\n")
-         ListFoldersFeature(conn, reportF)
+    for oldSrc in SourcesToLump:
+    # let's just do one
+   # oldSrc = 66
+        print (oldSrc)
+        Convert (conn, oldSrc, NewSourceID)
 
-      reportF.write ("\n")
-      reportF.write (G_Divider + "\nEnd of report \n")
-  return 0
+    return 0
 
 
 # ================================================================
--- RM -LumpSources/ReadMe.txt
+++ RM -LumpSources/ReadMe.txt
@@ -44,30 +44,85 @@ Make it specific for SSDI and SSACI
 So use it to create 2 sources- SSDI and SSACI
 assume that the data will come from ancestry
 
-Source Name
-File Number
-Subject
-Access Date
+Source Name		SSDIdb ANC US
+DataType		SSDI or SSACI
+Owner
+WebsiteTitle
+URL
+DatabaseName	Ancestyr database name for SSDI
+Access Date		when source was updated
 
-Source Text
+Source Text		full info about database
 Source Comment
 
-Media
-Repositories
-Web Tags
-Used
+Media			NULL
+Repositories	Ancestry repo
+Web Tags		URL of ancestry database
+Used			<output>
 
 In citation
-Research Note
-Detail Comment
+Name
+BirthDate
+Number
+SSDate		
+CitationDate
+AccessType
+
+
+Research Note	full listing of record
+Detail Comment	any notes relating to src
 
 
 =======================================
 Conversion process
 
+Figure out which group of sources will be lumped.
+do a select based on name and templateID 
+
+Get the SourceID of the source to get citations
+Let's assume all CD fields are blank and there is no citation in old source.
 
 Fill in its 
-SourceName = Citation Name
-
+Citation Name = SourceName
+Name		Name
+BirthDate	get it from SSDI
+Number		get it from SSDI
+SSDate		get it from SSDI
+CitationDate	copy from old Src
+AccessType		"downloaded"
 
 need to deal with XML fields
+
+
+have an old srcID and new srcID what next
+
+check how many citations linked to oldSrcID. stop if >1, skip if 0
+
+get info from oldSrc	XML
+
+Move the citation to newSrcID
+fill it in with old info	XML
+
+
+
+
+
+
+
+
+if old source has more than one citation stop
+or has 0 citations, skip it
+This will change when other sources are lumped but makes sense for the SSDI specific conversion
+
+After an old src is processed, delete it.(it should have 0 citations 
+and web links, becasue they should have been chnaged already.)
+
+Deal with Issue state- can be railroad worker-  e.g. SSDI Centron, Michael b1918
+
+How does the source I created handle different web site sources??
+each website would need a diff source, but with same template.
+
+WOULD BE NICE TO HAVE A FIELD FOR SSACI THAT INDICATE LSTING OF PARENTS.
+
+Citation table 
+<Root><Fields><Field><Name>CD</Name><Value></Value></Field></Fields></Root>
