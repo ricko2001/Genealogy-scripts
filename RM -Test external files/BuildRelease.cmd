eee96ca69d76e18d34fb0241a46d88b4b599fd05
Richard Otter <rjo7rjo7@gmail.com>
2023-04-19 11:56:39 -0700

    add more comments to make releaseprocess easier

--- RM -Test external files/BuildRelease.cmd
+++ RM -Test external files/BuildRelease.cmd
@@ -1,7 +1,12 @@
 @ECHO OFF
 
-REM Update all files in local git
-REM Test script.py, version.py, readme, hash
+ECHO Update all files in local git - make sure main branch is correct.
+ECHO Test script.py, version.py, readme.txt
+ECHO push code to github main
+
+ECHO This script is to be run from the "RM -Test external files" folder
+ECHO It will create a subfolder -  Release TestExternalFiles vn.n.n.n where all building is done.
+ECHO That subfolder should be moved to the upper level Releases folder when ready.
 
 REM ECHO Enter version number n.n.n.n
 SET /P  VERSION_NUMBER="Enter version number n.n.n.n   "
@@ -41,7 +46,25 @@ cd ..
 
 pause
 
-ECHO for release- use git tag name = %APPNAME%_v%VERSION_NUMBER%
+ECHO for RELEASE on GitHub- 
+ECHO  Tag the local repo with an annotated tag  - no spaces allowed in tag name
+ECHO  use git tag name = %APPNAME%_v%VERSION_NUMBER%
+ECHO  push the local tag to github main
+ECHO  tags can be deleted and or renamed-  see   https://phoenixnap.com/kb/git-rename-tag
+
+ECHO  Draft a release
+ECHO  Check previous title for patter (use spaces in title, not in tag)
+ECHO  use release title  = %APPNAME% v%VERSION_NUMBER%
+
+ECHO  Add release info, refer to previous descriptions for what to write.
+ECHO  Add the zip file
+ECHO  Save as draft and let it sit and age for a while
+ECHO  Publish the release
+ECHO  Write an announcement post to groups.
+ECHO  
+
+
+use git tag name = %APPNAME%_v%VERSION_NUMBER%
 
 pause
 
