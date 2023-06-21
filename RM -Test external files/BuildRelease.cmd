33c33c8e47585b7dfb6c187766088e90fa0558fc
Richard Otter <rjo7rjo7@gmail.com>
2023-06-21 14:56:42 -0700

    improve release procedure notes

--- RM -Test external files/BuildRelease.cmd
+++ RM -Test external files/BuildRelease.cmd
@@ -6,7 +6,7 @@ ECHO push code to github main
 
 ECHO This script is to be run from the "RM -Test external files" folder
 ECHO It will create a subfolder -  Release TestExternalFiles vn.n.n.n where all building is done.
-ECHO That subfolder should be moved to the upper level Releases folder when ready.
+ECHO That subfolder should be moved to the upper level Releases folder when done.
 
 REM ECHO Enter version number n.n.n.n
 SET /P  VERSION_NUMBER="Enter version number n.n.n.n   "
@@ -17,7 +17,7 @@ SET DIST_FLDR_NAME=%APPNAME% v%VERSION_NUMBER%
 
 SET REL_FLDR=Release %APPNAME% v%VERSION_NUMBER%
 
-ECHO Is the Version.py updated ?
+ECHO Is the Version.py updated in both places ?
 pause
 
 MKDIR ".\%REL_FLDR%"
@@ -39,33 +39,38 @@ move *.spec  .\build
 copy dist\*.exe .
 move build ..
 move dist ..
-
 cd ..
 
 7za a -tzip "%DIST_FLDR_NAME%.zip" "%DIST_FLDR_NAME%"
 
 pause
-
-ECHO for RELEASE on GitHub- 
-ECHO  Tag the local repo with an annotated tag  - no spaces allowed in tag name
+ECHO  RELEASE PROCEDURE for GitHub
+ECHO  
 ECHO  use git tag name = %APPNAME%_v%VERSION_NUMBER%
+ECHO  
+ECHO  see -  https://git-scm.com/docs/git-tag
+ECHO  Tag the local repo with an annotated tag  - no spaces allowed in tag name
+ECHO  git tag --annotate %APPNAME%_v%VERSION_NUMBER%
+ECHO  write text in default text editor, save and close. Perhaps mini release notes?
+ECHO  
 ECHO  push the local tag to github main
+ECHO  git push origin %APPNAME%_v%VERSION_NUMBER%
+ECHO  
 ECHO  tags can be deleted and or renamed-  see   https://phoenixnap.com/kb/git-rename-tag
-
+ECHO  
 ECHO  Draft a release
-ECHO  Check previous title for patter (use spaces in title, not in tag)
+ECHO  Check previous title for pattern (use spaces in title, not in tag)
 ECHO  use release title  = %APPNAME% v%VERSION_NUMBER%
-
+ECHO  
 ECHO  Add release info, refer to previous descriptions for what to write.
 ECHO  Add the zip file
 ECHO  Save as draft and let it sit and age for a while
 ECHO  Publish the release
 ECHO  Write an announcement post to groups.
 ECHO  
-
-
-ECHO use git tag name = %APPNAME%_v%VERSION_NUMBER%
-
+ECHO  in Terminal use - Shift-Control-A,  Shift-Control-C  
+ECHO  and paste into a new Build_output.txt file, and put it into Release folder/
+ECHO  
 ECHO  ============================================
 ECHO  END OF SCRIPT
 ECHO  ============================================
