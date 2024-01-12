8169ba368341b05567dad066edb81890e16c380a
Richard Otter <rjo7rjo7@gmail.com>
2024-01-11 21:13:11 -0800

    updatees and name chane

---
+++ RM -Maintenance SQL/Maintenance run.cmd
@@ -0,0 +1,55 @@
+rem @ECHO OFF
+REM
+REM  File paths must use either doubled back slash due to Sqlite3.exe limitation
+
+REM file locations
+
+REM SET DATABASE=.\\DB\\TEST.rmtree
+SET DATABASE=..\\Otter-Saito.rmtree
+
+SET RMNOCASE=C:\\Users\\rotter\\Genealogy\\GeneDB\\SW\\unifuzz64.dll
+
+SET SQL_FILE=.\\Maintenance-auto.sql
+
+SET REPORT_FILE=..\\Report_Maintenance_Run.txt
+
+REM Generate time stamp
+FOR /F %%A IN ('WMIC OS GET LocalDateTime ^| FINDSTR \.') DO @SET B=%%A
+SET TIMESTAMP=%B:~0,4%-%B:~4,2%-%B:~6,2%-%B:~8,2%%B:~10,2%%B:~12,2%
+
+REM Create TempFile
+SET TEMPFILE=%TEMP%\MaintenanceTempFile_%TIMESTAMP%.txt
+
+if exist "%REPORT_FILE%" del  "%REPORT_FILE%"
+
+REM Create the command file for sqlite3
+
+echo .output "%REPORT_FILE%" > "%TEMPFILE%"
+echo .changes on             >> "%TEMPFILE%"
+echo .bail on                >> "%TEMPFILE%"
+echo .open "%DATABASE%"      >> "%TEMPFILE%"
+echo .load "%RMNOCASE%"      >> "%TEMPFILE%"
+echo .echo on                >> "%TEMPFILE%"
+echo .version                >> "%TEMPFILE%"
+echo .                       >> "%TEMPFILE%"
+echo .                       >> "%TEMPFILE%"
+echo .                       >> "%TEMPFILE%"
+echo --Database="%DATABASE%" >> "%TEMPFILE%"
+echo --Date %TIMESTAMP%      >> "%TEMPFILE%"
+echo --File "%SQL_FILE%"     >> "%TEMPFILE%"
+copy "%TEMPFILE%" +"%SQL_FILE%" "%TEMPFILE%"
+echo .quit                   >> "%TEMPFILE%"
+
+
+REM echo =======================
+REM echo contents of TEMPFILE
+REM type "%TEMPFILE%"
+
+SQLite3 < "%TEMPFILE%"
+
+
+REM delete the temp file
+del "%TEMPFILE%"
+
+REM Display the report file in NotePad++
+"%PROGRAMFILES%\notepad++\notepad++.exe" "%REPORT_FILE%"
