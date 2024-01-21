5c69c69ac5696ba8f95343903f85e9e132c270c8
Richard Otter <rjo7rjo7@gmail.com>
2024-01-21 13:43:54 -0800

    handle more cases

--- RM -CitationSortOrder/CitationSortOrder.py
+++ RM -CitationSortOrder/CitationSortOrder.py
@@ -14,16 +14,7 @@ import hashlib
 ##   RM-Python-config.ini  ( Configuration ini text file to set options and parameters)
 ##   Python v3.9 or greater
 
-#   Transfer a citation from old source to new source -only if sources use same template
-#   input citation name
-#   confirm there is only 1 match to like input%
-#   display existing source name
-#   input new source name
-#   confirm there is only 1 match to like input%
-#   confirm old and new source both use same template
-#   ask for confirm
-#   change the owning source
-
+#   Re-order citations attached to a particular fact.
 
 
 # ===================================================DIV60==
@@ -68,76 +59,179 @@ def main():
   print("\nDatabase = " + os.path.abspath(database_Path) + "\n")
 
 
-  # input the PersonID  RIN and type
-  PersonID = input("Enter the RIN:\n")
-  FactTypeID = input("Enter the FactTypeID:\n")
+  # input the PersonID  RIN
+  PersonID = input("Enter the RIN of the person who has the citations:\n")
 
-  SqlStmt = """
-   SELECT COUNT(), et.EventID
-    FROM EventTable AS et
-    WHERE et.OwnerID = ?
-      AND et.OwnerType = 0
-      AND et.EventType = ?
-   """
+  attacdhedTo = input("Are the citations attached to a Fact (f), a name (n) or the Person (p)?:\n")
 
-  cur = dbConnection.cursor()
-  cur.execute( SqlStmt, (PersonID, FactTypeID) )
-  row = cur.fetchone()
+# ===========================================DIV50==
+  if attacdhedTo in "P p Person person":
 
-  numberOfEvents = row[0]
-  EventID = row[1]
-
-
-  if (numberOfEvents > 1):
-    PauseWithMessage('Found more than 1 event. Try again.')
-    return 1
-  if (numberOfEvents == 0):
-    PauseWithMessage('Event not found. Try again.')
-    return 1
-
-
-  SqlStmt = """
-  SELECT clt.SortOrder, clt.LinkID, st.Name, ct.CitationName
-    FROM CitationTable AS ct
-    JOIN CitationLinkTable AS clt ON clt.CitationID = ct.CitationID
-    JOIN SourceTable AS st ON ct.SourceID = st.SourceID
-    WHERE clt.OwnerID = ?
-      AND clt.OwnerType = 2
-  ORDER BY clt.SortOrder ASC
-    """
-  cur = dbConnection.cursor()
-  cur.execute( SqlStmt, (EventID, ) )
-  rows = cur.fetchall()
+    SqlStmt = """
+    SELECT clt.SortOrder, clt.LinkID, st.Name, ct.CitationName
+      FROM CitationTable AS ct
+      JOIN CitationLinkTable AS clt ON clt.CitationID = ct.CitationID
+      JOIN SourceTable AS st ON ct.SourceID = st.SourceID
+      WHERE clt.OwnerID = ?
+        AND clt.OwnerType = 0
+    ORDER BY clt.SortOrder ASC
+      """
+    cur = dbConnection.cursor()
+    cur.execute( SqlStmt, (PersonID, ) )
+    rows = cur.fetchall()
   
+    if len(rows) == 0:
+      PauseWithMessage("Either RIN does not exist or that person has no citations attached.")
+      return 1
+
+# ===========================================DIV50==
+  elif attacdhedTo in "F f Fact fact":
+  
+    FactTypeID = input("Enter the FactTypeID:\n")
+  
+    SqlStmt = """
+     SELECT COUNT(), et.EventID
+      FROM EventTable AS et
+      WHERE et.OwnerID = ?
+        AND et.OwnerType = 0
+        AND et.EventType = ?
+     """
+  
+    cur = dbConnection.cursor()
+    cur.execute( SqlStmt, (PersonID, FactTypeID) )
+    row = cur.fetchone()
+  
+    numberOfEvents = row[0]
+    EventID = row[1]
+  
+  
+    if (numberOfEvents > 1):
+      PauseWithMessage('Found more than 1 event. Try again.')
+      return 1
+    if (numberOfEvents == 0):
+      PauseWithMessage('Event not found. Try again.')
+      return 1
+  
+  
+    SqlStmt = """
+     SELECT clt.SortOrder, clt.LinkID, st.Name, ct.CitationName
+      FROM CitationTable AS ct
+      JOIN CitationLinkTable AS clt ON clt.CitationID = ct.CitationID
+      JOIN SourceTable AS st ON ct.SourceID = st.SourceID
+      WHERE clt.OwnerID = ?
+        AND clt.OwnerType = 2
+     ORDER BY clt.SortOrder ASC
+    """
+    cur = dbConnection.cursor()
+    cur.execute( SqlStmt, (EventID, ) )
+    rows = cur.fetchall()
+    
+# ===========================================DIV50==
+  elif attacdhedTo in "N n Name name":
+  
+    SqlStmt = """
+     SELECT COUNT(), et.NameID
+      FROM EventTable AS et
+      WHERE et.OwnerID = ?
+        AND et.OwnerType = 0
+        AND et.EventType = ?
+    """
+  
+    cur = dbConnection.cursor()
+    cur.execute( SqlStmt, (PersonID, FactTypeID) )
+    row = cur.fetchone()
+  
+    numberOfEvents = row[0]
+    EventID = row[1]
+  
+  
+    if (numberOfEvents > 1):
+      PauseWithMessage('Found more than 1 event. Try again.')
+      return 1
+    if (numberOfEvents == 0):
+      PauseWithMessage('Event not found. Try again.')
+      return 1
+
+
+    SqlStmt = """
+     SELECT clt.SortOrder, clt.LinkID, st.Name, ct.CitationName
+       FROM CitationTable AS ct
+       JOIN CitationLinkTable AS clt ON clt.CitationID = ct.CitationID
+       JOIN SourceTable AS st ON ct.SourceID = st.SourceID
+       WHERE clt.OwnerID = ?
+         AND clt.OwnerType = 7
+     ORDER BY clt.SortOrder ASC
+     """
+    cur = dbConnection.cursor()
+    cur.execute( SqlStmt, (EventID, ) )
+    rows = cur.fetchall()
+
+# ===========================================DIV50==
+  else:
+    PauseWithMessage("Cannot interpret response.")
+    return 1
+
+
+# ===========================================DIV50==
+# Do the re-order
+
   rowDict = dict()
-
+  # Create the origin 1 based dictionary
+  # Use 1 based indexing for human users
   for i in range( 0, len(rows)):
-    rowDict[i] =( (rows[i][1], (rows[i][2], rows[i][3])))
+    rowDict[i+1] =( (rows[i][1], (rows[i][2], rows[i][3])))
 
-  # Print the initial order
-  for i in range( 0, len(rowDict)):
+  print ("\n\n")
+
+  # range limit when using 1 based indexing
+  citNumberLimit = len(rowDict) +1
+
+  # Print the initial order (matches default in RM)
+  for i in range( 1, citNumberLimit):
     print( i, rowDict[i][1] )
+  print ( "\n" +
+          "To re-order citations, at each prompt, enter one of:\n"+
+          "*  the number of the current citation that should go into this slot\n" +
+          "*  nothing- to accept current slot as correct\n" +
+          "*  s to accept current and following slots as correct\n")
 
   Done = False
   while not Done:
-    print ("\n\n")
-
-    for j in range( 0, len(rowDict)-1):
-      firstStr =  input( "position # (or s)" + str(j) + " : ")
-      if firstStr == '': firstStr = j
-      if firstStr in 'Ss': break
-      swapVal = int(firstStr)
+    for j in range( 1, citNumberLimit-1):
+      response =  str(input( "for slot # " + str(j) + " : "))
+      if response == '': continue
+      elif response in 'S s': break
+      else : 
+        try:
+          swapVal = int(response) 
+        except ValueError:
+          print('Please enter an integer, blank or S or s')
+          return 1
       rowDict[swapVal], rowDict[j] = rowDict[j], rowDict[swapVal]
-      for i in range( 0, len(rowDict)):
+      print ("\n\n")
+      for i in range( 1, citNumberLimit):
         print( i, rowDict[i][1] )
 
     print ("\n\n")
 
-    # Print order after round of sorting
-    for i in range( 0, len(rowDict)):
+    # Print order after a round of sorting
+    for i in range( 1, citNumberLimit):
       print( i, rowDict[i][1] )
     
-    if input("Finished ? Yy/Nn") in "Yy": Done = True
+    respponse = input("\n\n" +
+                      "Are you satisfied with the citation order shown above?\n" +
+                      "Enter one of-\n" +
+                      "*  Y/y to make the citation order change as shown above\n" +
+                      "*  N/n to go back and do another round of re-ordering\n"+
+                      "*  A/a to abort and not make any changes to the database \n")
+    if respponse  in "Yy":
+      Done = True
+    elif respponse  in "Aa":
+      PauseWithMessage("No changes made to database")
+      return 1
+    # assume No
+  # End while Done
+
 
   # Now update the SortOrder column for the given Citation Links 
   SqlStmt = """
@@ -145,7 +239,7 @@ def main():
     SET SortOrder = ? 
     WHERE LinkID = ?
     """
-  for i in range( 0, len(rowDict)):
+  for i in range( 1, citNumberLimit):
     cur = dbConnection.cursor()
     cur.execute( SqlStmt, (i, rowDict[i][0]) )
     dbConnection.commit()
@@ -173,9 +267,7 @@ def create_DBconnection(db_file_path):
       dbConnection = sqlite3.connect(db_file_path)
     except Error as e:
         print(e)
-        print("\n\n")
-        print( "Cannot open the RM database file. \n")
-        input("Press the <Enter> key to exit...")
+        PauseWithMessage( "\n\nCannot open the RM database file. \n")
         sys.exit()
     return dbConnection
 
