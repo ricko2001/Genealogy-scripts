fb7b153d73e58f06f8ac0201252e7f2f502d05ad
Richard Otter <RichardJOtter@gmail.com>
2024-05-26 15:56:08 -0700

    new-  search for Role use

---
+++ RM -SQL for creating useful groups/People with a shared specific fact and role.sql
@@ -0,0 +1,14 @@
+  -- Query on Role Name and optionally, FactType Name
+  WITH
+  constants(C_FactName, C_RoleName) AS (
+     SELECT   '%'             AS C_FactName,
+              'spouse'        AS C_RoleName
+    )
+  SELECT pt.PersonID
+    FROM PersonTable       AS pt
+  INNER JOIN WitnessTable  AS wt  ON wt.PersonID = pt.PersonId
+  INNER JOIN RoleTable     AS rt  ON rt.RoleID = wt.Role
+  INNER JOIN FactTypeTable AS ftt ON ftt.FactTypeID = rt.EventType
+  WHERE ftt.Name     LIKE((SELECT C_FactName FROM constants))
+    AND  rt.RoleName LIKE((SELECT C_RoleName FROM constants))
+
