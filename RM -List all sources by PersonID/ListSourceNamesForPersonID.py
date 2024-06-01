27aefdfc2054fb117622f1348579e271110a9bab
Richard Otter <RichardJOtter@gmail.com>
2024-06-01 11:01:49 -0700

    Now displays to report file reformat, bring up to std

--- RM -List all sources by PersonID/ListSourceNamesForPersonID.py
+++ RM -List all sources by PersonID/ListSourceNamesForPersonID.py
@@ -1,150 +1,359 @@
-import sqlite3
-from sqlite3 import Error
-import sys
 import os
-import time
+import sys
+import sqlite3
 import configparser
+from datetime import datetime
+import subprocess
+import traceback
+
+# List all citations associated with a Person
+
+# Tested with: 
+#   RootsMagic database file v9.1.6
+#   Python for Windows v3.45.1
+#   unifuzz64.dll (MD5=06a1f485b0fae62caa80850a8c7fd7c2)
 
 
-## Tested with RootsMagic v8.1.5    (RM7 no longer supported)
-##             Python for Windows v3.10.2
-##             unifuzz64.dll (ver not set, MD5=06a1f485b0fae62caa80850a8c7fd7c2)
 
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
+# ===================================================DIV60==
+def main():
+
+    # Configuration
+    config_file_name = "RM-Python-config.ini"
+    db_connection = None
+    report_display_app = None
+
+    # ===========================================DIV50==
+    # Errors go to console window
+    # ===========================================DIV50==
     try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
+        # config file must be in "current directory" and encoded as UTF-8 (no BOM).
+        # see   https://docs.python.org/3/library/configparser.html
+        config_file_path = os.path.join(
+            get_current_directory(), config_file_name)
 
-    return conn
+        # Check that config file is at expected path and that it is readable & valid.
+        if not os.path.exists(config_file_path):
+            raise RM_Py_Exception("ERROR: The configuration file, " + config_file_name
+                                  + " must be in the same directory as the .py or .exe file.\n\n")
+
+        config = configparser.ConfigParser(empty_lines_in_values=False,
+                                           interpolation=None)
+        try:
+            config.read(config_file_path, 'UTF-8')
+        except:
+            raise RM_Py_Exception("ERROR: The " + config_file_name
+                                  + " file contains a format error and cannot be parsed.\n\n")
+        try:
+            report_path = config['FILE_PATHS']['REPORT_FILE_PATH']
+        except:
+            raise RM_Py_Exception('ERROR: REPORT_FILE_PATH must be defined in the '
+                                  + config_file_name + "\n\n")
+        try:
+            # Use UTF-8 encoding for the report file. Test for write-ability
+            open(report_path,  mode='w', encoding='utf-8')
+        except:
+            raise RM_Py_Exception('ERROR: Cannot create the report file '
+                                  + report_path + "\n\n")
+
+    except RM_Py_Exception as e:
+        pause_with_message(e)
+        return 1
+    except Exception as e:
+        traceback.print_exception(e, file=sys.stdout)
+        pause_with_message(
+            "ERROR: Application failed. Please email report.\n\n " + str(e)
+            + "\n\n to the author")
+        return 1
+
+    # open the already tested report file
+    report_file = open(report_path,  mode='w', encoding='utf-8')
+
+    # ===========================================DIV50==
+    # Errors from here forward, go to Report File
+    # ===========================================DIV50==
+    try:
+        try:
+            report_display_app = config['FILE_PATHS']['REPORT_FILE_DISPLAY_APP']
+        except:
+            pass
+        if report_display_app is not None and not os.path.exists(report_display_app):
+            raise RM_Py_Exception('ERROR: Path for report file display app not found: '
+                                  + report_display_app)
+
+        try:
+            database_path = config['FILE_PATHS']['DB_PATH']
+        except:
+            raise RM_Py_Exception('ERROR: DB_PATH must be specified.')
+        if not os.path.exists(database_path):
+            raise RM_Py_Exception('ERROR: Path for database not found: ' + database_path
+                                  + '\n\n' 'Absolute path checked:\n"'
+                                  + os.path.abspath(database_path) + '"')
+        
+        # Always need RMNOCASE for creating a new group (tag name index)
+        try:
+            rmnocase_path = config['FILE_PATHS']['RMNOCASE_PATH']
+        except:
+            raise RM_Py_Exception('ERROR: RMNOCASE_PATH must be specified.')
+        if rmnocase_path is not None and not os.path.exists(rmnocase_path):
+            raise RM_Py_Exception('ERROR: Path for RMNOCASE extension (unifuzz64.dll) not found: '
+                            + rmnocase_path
+                            + '\n\n' 'Absolute path checked:\n"'
+                              + os.path.abspath(rmnocase_path) + '"')
+
+        # RM database file info
+        file_modification_time = datetime.fromtimestamp(
+            os.path.getmtime(database_path))
+
+        db_connection = create_db_connection(database_path, rmnocase_path)
+
+        # write header to report file
+        report_file.write("Report generated at      = " + time_stamp_now()
+                          + "\n" "Database processed       = "
+                          + os.path.abspath(database_path)
+                          + "\n" "Database last changed on = "
+                          + file_modification_time.strftime("%Y-%m-%d %H:%M:%S")
+                          + "\n" "SQLite library version   = "
+                          + get_SQLite_library_version(db_connection) + "\n\n\n\n")
+
+        display_sources_feature(config, db_connection, report_file)
+
+    except (sqlite3.OperationalError, sqlite3.ProgrammingError) as e:
+        report_file.write(
+            "ERROR: SQL execution returned an error \n\n" + str(e))
+        return 1
+    except RM_Py_Exception as e:
+        report_file.write(str(e))
+        return 1
+    except Exception as e:
+        traceback.print_exception(e, file=report_file)
+        report_file.write("\n\n"
+                          "ERROR: Application failed. Please email report file to author. ")
+        return 1
+    finally:
+        if db_connection is not None:
+            db_connection.commit()
+            db_connection.close()
+        report_file.close()
+        if report_display_app is not None:
+            subprocess.Popen([report_display_app, report_path])
+    return 0
 
 
-def select(conn, PersonID):
+# ===================================================DIV60==
+def display_sources_feature(config, db_connection, report_file):
 
-    SqlStmt="""\
---- PERSON sources
-  SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
-    FROM SourceTable
-    JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
-    JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
-    JOIN PersonTable       ON CitationLinkTable.OwnerID = PersonTable.PersonID
-    WHERE PersonTable.PersonID=?
-      AND CitationLinkTable.OwnerType=0
+    PersonID = None
+    try:
+        PersonID = config['RIN']['PERSON_RIN']
+    except:
+        pass
+    
+    if PersonID is None:
+        PersonID = input("\n"  "PersonID/RIN =")
 
-  UNION
 
---- NAME sources
-  SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
-    FROM SourceTable
-    JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
-    JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
-    JOIN NameTable         ON CitationLinkTable.OwnerID = NameTable.NameID
-    WHERE NameTable.OwnerID=?
-      AND CitationLinkTable.OwnerType=7
+    SqlStmt_orig="""\
+-- PERSON sources
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  JOIN PersonTable       ON CitationLinkTable.OwnerID = PersonTable.PersonID
+  WHERE PersonTable.PersonID=?
+    AND CitationLinkTable.OwnerType=0
 
-  UNION
+UNION
+
+-- NAME sources
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  JOIN NameTable         ON CitationLinkTable.OwnerID = NameTable.NameID
+  WHERE NameTable.OwnerID=?
+    AND CitationLinkTable.OwnerType=7
+
+UNION
 
 --- EVENT-PERSON sources
-  SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
-    FROM SourceTable
-    JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
-    JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
-    JOIN EventTable ON CitationLinkTable.OwnerID = EventTable.EventID
-    WHERE EventTable.OwnerID=?
-      AND CitationLinkTable.OwnerType=2
-      AND EventTable.OwnerType=0
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  JOIN EventTable ON CitationLinkTable.OwnerID = EventTable.EventID
+  WHERE EventTable.OwnerID=?
+    AND CitationLinkTable.OwnerType=2
+    AND EventTable.OwnerType=0
 
-  UNION
+UNION
 
---- EVENT-FAMILY sources
-  SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
-    FROM SourceTable
-    JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
-    JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
-    JOIN EventTable        ON CitationLinkTable.OwnerID = EventTable.EventID
-    JOIN FamilyTable       ON EventTable.OwnerID = FamilyTable.FamilyID
-    WHERE (FamilyTable.FatherID=? OR FamilyTable.MotherID=?)
-      AND CitationLinkTable.OwnerType=2
-      AND EventTable.OwnerType=1
+---- EVENT-FAMILY sources
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  JOIN EventTable        ON CitationLinkTable.OwnerID = EventTable.EventID
+  JOIN FamilyTable       ON EventTable.OwnerID = FamilyTable.FamilyID
+  WHERE (FamilyTable.FatherID=? OR FamilyTable.MotherID=?)
+    AND CitationLinkTable.OwnerType=2
+    AND EventTable.OwnerType=1
 
-  UNION
+UNION
 
 --- FAMILY sources
-  SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
-    FROM SourceTable
-    JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
-    JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
-    JOIN FamilyTable       ON CitationLinkTable.OwnerID = FamilyTable.FamilyID
-    WHERE (FamilyTable.FatherID=? OR FamilyTable.MotherID=?)
-      AND CitationLinkTable.OwnerType=1
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  JOIN FamilyTable       ON CitationLinkTable.OwnerID = FamilyTable.FamilyID
+  WHERE (FamilyTable.FatherID=? OR FamilyTable.MotherID=?)
+    AND CitationLinkTable.OwnerType=1
+    
+ORDER BY SourceTable.Name;
+"""
 
-  ORDER BY SourceTable.Name;
+    SqlStmt="""\
+--      PERSON sources
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  WHERE CitationLinkTable.OwnerID=?
+    AND CitationLinkTable.OwnerType=0
+
+UNION
+
+--      NAME sources
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  JOIN NameTable         ON CitationLinkTable.OwnerID = NameTable.NameID
+  WHERE NameTable.OwnerID=?
+    AND CitationLinkTable.OwnerType=7
+
+UNION
+
+--      EVENT-PERSON sources
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  JOIN EventTable ON CitationLinkTable.OwnerID = EventTable.EventID
+  WHERE EventTable.OwnerID=?
+    AND CitationLinkTable.OwnerType=2
+    AND EventTable.OwnerType=0
+
+UNION
+
+--      EVENT-FAMILY sources
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  JOIN EventTable        ON CitationLinkTable.OwnerID = EventTable.EventID
+  JOIN FamilyTable       ON EventTable.OwnerID = FamilyTable.FamilyID
+  WHERE (FamilyTable.FatherID=? OR FamilyTable.MotherID=?)
+    AND CitationLinkTable.OwnerType=2
+    AND EventTable.OwnerType=1
+
+UNION
+
+--      FAMILY sources
+SELECT DISTINCT SourceTable.Name, CitationTable.CitationName
+  FROM SourceTable
+  JOIN CitationTable     ON SourceTable.SourceID = CitationTable.SourceID
+  JOIN CitationLinkTable ON CitationTable.CitationID = CitationLinkTable.LinkID
+  JOIN FamilyTable       ON CitationLinkTable.OwnerID = FamilyTable.FamilyID
+  WHERE (FamilyTable.FatherID=? OR FamilyTable.MotherID=?)
+    AND CitationLinkTable.OwnerType=1
+    
+ORDER BY SourceTable.Name;
 """
 
 
-    cur = conn.cursor()
+    cur = db_connection.cursor()
     cur.execute(SqlStmt, (PersonID,PersonID,PersonID,PersonID,PersonID,PersonID,PersonID) )
-
     rows = cur.fetchall()
 
+    report_file.write("PersonID = " + str(PersonID) + "\n")
+    report_file.write (str(len(rows)) + " source citations found \n\n")
+
     for row in rows:
-        print(row[0], "\t\t", row[1])
-    print ("\n", len(rows), " source citations found \n\n")
+        report_file.write(row[0] + "\t\t" + row[1] + "\n\n")
 
-    return rows
-
+    return
 
 
-def main():
-  # Configuration file
-  IniFile="RM-Python-config.ini"
-  
-  # ini file must be in "current directory" and encoded as UTF-8 if non-ASCII chars present (no BOM)
-  if not os.path.exists(IniFile):
-      print("ERROR: The ini configuration file, " + IniFile + " must be in the current directory." )
-      return
+# ===================================================DIV60==
+def create_db_connection(db_file_path, db_extension_file_path):
 
-  config = configparser.ConfigParser()
-  config.read(IniFile, 'UTF-8')
-
-  # Read file paths from ini file
-# TODO  report_Path   = config['File Paths']['REPORT_PATH']
-  database_Path = config['FILE_PATHS']['DB_PATH']
-  RMNOCASE_Path = config['FILE_PATHS']['RMNOCASE_PATH']
-
-  if not os.path.exists(database_Path):
-      print('Database path not found')
-      return
-
-  FileModificationTime = time.ctime( os.path.getmtime(database_Path))
-
-  PersonID=0
-  try:
-   PersonID = config['RIN']['PERSON_RIN']
-  except KeyError:
-    PersonID = input("No ID specified in configuration file. Enter it now: ")
-
-  print("\n\nPersonID=", PersonID, "\n\n")
-
-  # create a database connection
-  conn = create_connection(database_Path)
-  conn.enable_load_extension(True)
-  conn.load_extension(RMNOCASE_Path)
-  
-  with conn:
-      select(conn, PersonID)
-
-  input("Hit Enter to exit")
+    dbConnection = None
+    try:
+        dbConnection = sqlite3.connect(db_file_path)
+        if db_extension_file_path is not None:
+            # load SQLite extension
+            dbConnection.enable_load_extension(True)
+            dbConnection.load_extension(db_extension_file_path)
+    except Exception as e:
+        raise RM_Py_Exception(
+            e, "\n\n" "Cannot open the RM database file." "\n")
+    return dbConnection
 
 
+# ===================================================DIV60==
+def get_SQLite_library_version(dbConnection):
+
+    # returns a string like 3.42.0
+    SqlStmt = "SELECT sqlite_version()"
+    cur = dbConnection.cursor()
+    cur.execute(SqlStmt)
+    return cur.fetchone()[0]
 
 
+# ===================================================DIV60==
+def time_stamp_now(type=""):
+
+    # return a TimeStamp string
+    now = datetime.now()
+    if type == '':
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    elif type == 'file':
+        dt_string = now.strftime("%Y-%m-%d_%H%M%S")
+    return dt_string
+
+
+# ===================================================DIV60==
+def pause_with_message(message=None):
+
+    if (message != None):
+        print(str(message))
+    input("\n" "Press the <Enter> key to continue...")
+    return
+
+
+# ===================================================DIV60==
+def get_current_directory():
+
+    # Determine if application is a script file or frozen exe and get its directory
+    # see   https://pyinstaller.org/en/stable/runtime-information.html
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        application_path = os.path.dirname(sys.executable)
+    else:
+        application_path = os.path.dirname(__file__)
+    return application_path
+
+
+# ===================================================DIV60==
+class RM_Py_Exception(Exception):
+
+    '''Exceptions thrown for configuration/database issues'''
+
+
+# ===================================================DIV60==
+# Call the "main" function
 if __name__ == '__main__':
     main()
-    
+
+# ===================================================DIV60==
