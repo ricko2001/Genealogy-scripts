fbdc74b08e5c3bdf371c3b30c08d32bcdc06d93e
Richard Otter <RichardJOtter@gmail.com>
2024-06-02 21:34:57 -0700

    refactor

--- RM -Group from SQL/GroupFromSQL.py
+++ RM -Group from SQL/GroupFromSQL.py
@@ -6,175 +6,162 @@ import configparser
 import subprocess
 import traceback
 
-
 # Always make a database backup before using this script.
 
-# Requirements: (see ReadMe.txt for details)
-# RootsMagic v9 database file
-# RM-Python-config.ini  (Configuration ini file to set options and parameters)
-# unifuzz64.dll optionally needed depending on the SQL run
-# Python v3.9 or greater
+# Requirements:
+#   RootsMagic database file
+#   RM-Python-config.ini
+
+# Tested with: 
+#   RootsMagic database file v9.1.6
+#   Python for Windows v3.12.3
+
+# Config files fields used
+#    FILE_PATHS  REPORT_FILE_PATH
+#    FILE_PATHS  REPORT_FILE_DISPLAY_APP
+#    FILE_PATHS  DB_PATH
+#    FILE_PATHS  RMNOCASE_PATH
+#
+#    OPTIONS     GROUP_FROM_SQL_OPTION_SET
+#
+#    <value of G_F_S_O_S>     RM_GROUP_NAME
+#    <value of G_F_S_O_S>     UPDATE_GROUP
+#    <value of G_F_S_O_S>     SQL_QUERY
+#    <value of G_F_S_O_S>     UPDATE_GROUP
+
 
 # ===================================================DIV60==
 def main():
 
     # Configuration
-    ini_file_name = "RM-Python-config.ini"
+    config_file_name = "RM-Python-config.ini"
     db_connection = None
-    rmnocase_path = None
     report_display_app = None
+    RMNOCASE_required = True
+    allow_db_changes = True
 
     # ===========================================DIV50==
     # Errors go to console window
     # ===========================================DIV50==
-    try:  # errors go to console window
-        # ini file must be in "current directory" and encoded as UTF-8 (no BOM).
+    try:
+        # config file must be in "current directory" and encoded as UTF-8 (no BOM).
         # see   https://docs.python.org/3/library/configparser.html
-        ini_file = os.path.join(get_current_directory(), ini_file_name)
+        config_file_path = os.path.join(
+            get_current_directory(), config_file_name)
 
-        # Check that ini file is at expected path and that it is readable & valid.
-        if not os.path.exists(ini_file):
-            raise RMPyExcep("ERROR: The ini configuration file, " + ini_file_name
-                            + " must be in the same directory as the .py or .exe file.\n\n")
+        # Check that config file is at expected path and that it is readable & valid.
+        if not os.path.exists(config_file_path):
+            raise RM_Py_Exception(
+                "ERROR: The configuration file, " + config_file_name
+                + " must be in the same directory as the .py or .exe file." "\n\n")
 
         config = configparser.ConfigParser(empty_lines_in_values=False,
                                            interpolation=None)
         try:
-            config.read(ini_file, 'UTF-8')
+            config.read(config_file_path, 'UTF-8')
         except:
-            raise RMPyExcep("ERROR: The " + ini_file_name
-                            + " file contains a format error and cannot be parsed.\n\n")
-
+            raise RM_Py_Exception(
+                "ERROR: The " + config_file_name
+                + " file contains a format error and cannot be parsed." "\n\n")
         try:
             report_path = config['FILE_PATHS']['REPORT_FILE_PATH']
         except:
-            raise RMPyExcep('ERROR: REPORT_FILE_PATH must be defined in the '
-                            + ini_file_name + "\n\n")
-
+            raise RM_Py_Exception(
+                'ERROR: REPORT_FILE_PATH must be defined in the '
+                + config_file_name + "\n\n")
         try:
             # Use UTF-8 encoding for the report file. Test for write-ability
             open(report_path,  mode='w', encoding='utf-8')
         except:
-            raise RMPyExcep('ERROR: Cannot create the report file '
-                            + report_path + "\n\n")
+            raise RM_Py_Exception('ERROR: Cannot create the report file '
+                                  + report_path + "\n\n")
 
-    except RMPyExcep as e:
+    except RM_Py_Exception as e:
         pause_with_message(e)
         return 1
     except Exception as e:
         traceback.print_exception(e, file=sys.stdout)
         pause_with_message(
-            "ERROR: Application failed. Please report.\n\n " + str(e))
+            "ERROR: Application failed. Please email error report:" "\n\n " +
+            str(e)
+            + "\n\n" "to the author")
         return 1
 
     # open the already tested report file
     report_file = open(report_path,  mode='w', encoding='utf-8')
+
     # ===========================================DIV50==
     # Errors from here forward, go to Report File
     # ===========================================DIV50==
-    try:        # errors go to the report file
+    try:
         try:
             report_display_app = config['FILE_PATHS']['REPORT_FILE_DISPLAY_APP']
         except:
             pass
         if report_display_app is not None and not os.path.exists(report_display_app):
-            raise RMPyExcep('ERROR: Path for report file display app not found: '
-                            + report_display_app)
+            raise RM_Py_Exception(
+                'ERROR: Path for report file display app not found: '
+                + report_display_app)
 
         try:
             database_path = config['FILE_PATHS']['DB_PATH']
         except:
-            raise RMPyExcep('ERROR: DB_PATH must be specified.')
+            raise RM_Py_Exception('ERROR: DB_PATH must be specified.')
         if not os.path.exists(database_path):
-            raise RMPyExcep('ERROR: Path for database not found: ' + database_path
-                            + '\n\n' 'Absolute path checked:\n"'
-                              + os.path.abspath(database_path) + '"')
+            raise RM_Py_Exception(
+                'ERROR: Path for database not found: ' + database_path
+                + '\n\n' 'Absolute path checked:\n"'
+                + os.path.abspath(database_path) + '"')
 
-        try:
-            rmnocase_path = config['FILE_PATHS']['RMNOCASE_PATH']
-        except:
-            # Always need RMNOCASE for creating a new group (tag name index)
-            raise RMPyExcep('ERROR: RMNOCASE_PATH must be specified.')
-        if rmnocase_path is not None and not os.path.exists(rmnocase_path):
-            raise RMPyExcep('ERROR: Path for RMNOCASE dll file not found: ' + rmnocase_path
-                            + '\n\n' 'Absolute path checked:\n"'
-                              + os.path.abspath(rmnocase_path) + '"')
-        try:
-            ActiveOptionsSection = config['OPTIONS']['GROUP_FROM_SQL_OPTION_SET']
-        except:
-            raise Exception(
-                'section: [OPTIONS],  key: GROUP_FROM_SQL_OPTION_SET   not found.')
-
-        if ActiveOptionsSection == '':
-            raise Exception('section: [OPTIONS], key: GROUP_FROM_SQL_OPTION_SET \n' +
-                            'No value entered.\n Nothing to do.')
-
-        try:
-            config[ActiveOptionsSection]
-        except:
-            raise Exception(
-                'section: [' + ActiveOptionsSection + ']   not found.')
-
-        try:
-            config[ActiveOptionsSection]['RM_GROUP_NAME']
-        except:
-            raise Exception(
-                'section: [' + ActiveOptionsSection + '],  key: RM_GROUP_NAME   not found.')
-
-        try:
-            config[ActiveOptionsSection]['UPDATE_GROUP']
-        except:
-            raise Exception(
-                'section: [' + ActiveOptionsSection + '],  key: UPDATE_GROUP   not found.')
-
-        try:
-            config[ActiveOptionsSection]['SQL_QUERY']
-        except:
-            raise Exception(
-                'section: [' + ActiveOptionsSection + '],  key: SQL_QUERY   not found.')
-
-        try:
-            config[ActiveOptionsSection].getboolean('UPDATE_GROUP')
-        except:
-            raise Exception(
-                'section: [' + ActiveOptionsSection + '],  key: UPDATE_GROUP value not interpretable.')
-
-
-
+        if RMNOCASE_required:
+            try:
+                rmnocase_path = config['FILE_PATHS']['RMNOCASE_PATH']
+            except:
+                raise RM_Py_Exception(
+                    'ERROR: RMNOCASE_PATH must be specified.')
+            if not os.path.exists(rmnocase_path):
+                raise RM_Py_Exception(
+                    'ERROR: Path for RMNOCASE extension (unifuzz64.dll) not found: '
+                    + rmnocase_path
+                    + '\n\n' 'Absolute path checked:\n"'
+                    + os.path.abspath(rmnocase_path) + '"')
 
         # RM database file info
-        FileModificationTime = datetime.fromtimestamp(
+        file_modification_time = datetime.fromtimestamp(
             os.path.getmtime(database_path))
 
-        db_connection = create_db_connection(database_path, rmnocase_path)
+        if RMNOCASE_required:
+            db_connection = create_db_connection(database_path, rmnocase_path)
+        else:
+            db_connection = create_db_connection(database_path, None)
 
         # write header to report file
-        report_file.write("Report generated at      = " + TimeStampNow()
+        report_file.write("Report generated at      = " + time_stamp_now()
                           + "\n" "Database processed       = "
                           + os.path.abspath(database_path)
                           + "\n" "Database last changed on = "
-                          + FileModificationTime.strftime("%Y-%m-%d %H:%M:%S")
+                          + file_modification_time.strftime("%Y-%m-%d %H:%M:%S")
                           + "\n" "SQLite library version   = "
-                          + GetSQLiteLibraryVersion(db_connection) + "\n\n\n\n")
+                          + get_SQLite_library_version(db_connection) + "\n\n\n\n")
 
+        run_selected_features(config, db_connection, report_file)
 
-
-        # Process the SQL and create the group
-        RunSQLGroupFeature(config, report_file, db_connection)
     except (sqlite3.OperationalError, sqlite3.ProgrammingError) as e:
-        report_file.write("ERROR: SQL execution returned an error \n\n" + str(e))
+        report_file.write(
+            "ERROR: SQL execution returned an error \n\n" + str(e))
         return 1
-    except RMPyExcep as e:
+    except RM_Py_Exception as e:
         report_file.write(str(e))
         return 1
     except Exception as e:
         traceback.print_exception(e, file=report_file)
-        report_file.write("\n\n"
-                          "ERROR: Application failed. Please email report file to author. ")
+        report_file.write(
+            "\n\n" "ERROR: Application failed. Please email report file to author. ")
         return 1
     finally:
         if db_connection is not None:
-            db_connection.commit()
+            if allow_db_changes:
+                db_connection.commit()
             db_connection.close()
         report_file.close()
         if report_display_app is not None:
@@ -183,25 +170,67 @@ def main():
 
 
 # ===================================================DIV60==
-def RunSQLGroupFeature(config, report_file,  dbConnection):
-    # config already validated, no error checking on config needed here
+def run_selected_features(config, db_connection, report_file):
 
-    ActiveOptionsSection = config['OPTIONS']['GROUP_FROM_SQL_OPTION_SET']
+    RunSQLGroupFeature(config, db_connection, report_file)
+
+
+# ===================================================DIV60==
+def RunSQLGroupFeature(config, db_connection, report_file):
+
+    try:
+        ActiveOptionsSection = config['OPTIONS']['GROUP_FROM_SQL_OPTION_SET']
+    except:
+        raise Exception(
+            'section: [OPTIONS],  key: GROUP_FROM_SQL_OPTION_SET   not found.')
+    try:
+        config[ActiveOptionsSection]
+    except:
+        raise Exception(
+            'section: [' + ActiveOptionsSection + ']   not found.')
+    try:
+        groupName = config[ActiveOptionsSection]['RM_GROUP_NAME']
+    except:
+        raise Exception(
+            'section: [' + ActiveOptionsSection + '],  key: RM_GROUP_NAME   not found.')
+    try:
+        config[ActiveOptionsSection]['UPDATE_GROUP']
+    except:
+        raise Exception(
+            'section: [' + ActiveOptionsSection + '],  key: UPDATE_GROUP   not found.')
+    try:
+        SQLvalue =config[ActiveOptionsSection]['SQL_QUERY']
+    except:
+        raise Exception(
+            'section: [' + ActiveOptionsSection + '],  key: SQL_QUERY   not found.')
+    try:
+        updateGroup = config[ActiveOptionsSection].getboolean('UPDATE_GROUP')
+    except:
+        raise Exception(
+            'section: [' + ActiveOptionsSection + '],  key: UPDATE_GROUP value not interpretable.')
+
 
     viewStmt = "DROP VIEW IF EXISTS PersonIdList_RJO_utils"
     try:
-        cur = dbConnection.cursor()
+        cur = db_connection.cursor()
         cur.execute(viewStmt)
     except Exception as e:
         # database open issues don't appear until here !
         raise Exception('Cannot open the database.\n' + str(e))
 
-    SQLvalue = config[ActiveOptionsSection]['SQL_QUERY']
+    if ActiveOptionsSection == '':
+        raise Exception('section: [OPTIONS], key: GROUP_FROM_SQL_OPTION_SET \n' +
+                        'No value entered.\n Nothing to do.')
+    
+    if SQLvalue == '':
+        raise Exception('section: [GROUP_FROM_SQL_OPTION_SET], key:  \n' +
+                        'No value entered.\n Nothing to do.')
+
 
     # generate the SQL statement and create the view
     SqlStmt = "CREATE TEMP VIEW PersonIdList_RJO_utils AS " + SQLvalue
     try:
-        cur = dbConnection.cursor()
+        cur = db_connection.cursor()
         cur.execute(SqlStmt)
     except:
         raise Exception("ERROR: Creating a VIEW on SQL_QUERY returned an error. \n" +
@@ -210,7 +239,7 @@ def RunSQLGroupFeature(config, report_file,  dbConnection):
     # errors in SQL show up here, not in view creation
     try:
         SqlStmt = "select count() from PersonIdList_RJO_utils"
-        cur = dbConnection.cursor()
+        cur = db_connection.cursor()
         cur.execute(SqlStmt)
         numInView = cur.fetchone()[0]
         report_file.write("# of persons selected: " + str(numInView) + "\n")
@@ -218,15 +247,12 @@ def RunSQLGroupFeature(config, report_file,  dbConnection):
         raise ("ERROR: SQL_QUERY returned an error when run as a VIEW. \n\n" +
                "SQL entered was:\n" + SQLvalue + "\n")
 
-    groupName = config[ActiveOptionsSection]['RM_GROUP_NAME']
-    updateGroup = config[ActiveOptionsSection].getboolean('UPDATE_GROUP')
-
-    CreateGroup(groupName, updateGroup, report_file, dbConnection)
+    CreateGroup(groupName, updateGroup, report_file, db_connection)
 
     report_file.write("\nSQL used to specify group membership:\n\n" + SQLvalue)
 
     viewStmt = "DROP VIEW IF EXISTS PersonIdList_RJO_utils"
-    cur = dbConnection.cursor()
+    cur = db_connection.cursor()
     cur.execute(viewStmt)
 
     return
@@ -338,21 +364,6 @@ def PopulateGroup(GroupID, dbConnection):
 
 
 # ===================================================DIV60==
-def create_db_connection(db_file_path, db_extension):
-
-    dbConnection = None
-    try:
-        dbConnection = sqlite3.connect(db_file_path)
-        if db_extension is not None and db_extension != '':
-            # load SQLite extension
-            dbConnection.enable_load_extension(True)
-            dbConnection.load_extension(db_extension)
-    except Exception as e:
-        raise RMPyExcep(e, "\n\n" "Cannot open the RM database file." "\n")
-    return dbConnection
-
-
-# ===================================================DIV60==
 def pause_with_message(message=None):
 
     if (message != None):
@@ -362,7 +373,7 @@ def pause_with_message(message=None):
 
 
 # ===================================================DIV60==
-def TimeStampNow(type=""):
+def time_stamp_now(type=""):
 
     # return a TimeStamp string
     now = datetime.now()
@@ -374,15 +385,13 @@ def TimeStampNow(type=""):
 
 
 # ===================================================DIV60==
-def GetSQLiteLibraryVersion(dbConnection):
+def get_SQLite_library_version(dbConnection):
 
     # returns a string like 3.42.0
     SqlStmt = "SELECT sqlite_version()"
     cur = dbConnection.cursor()
     cur.execute(SqlStmt)
     return cur.fetchone()[0]
-
-
 
 
 # ===================================================DIV60==
@@ -407,12 +416,12 @@ def create_db_connection(db_file_path, db_extension):
             db_connection.enable_load_extension(True)
             db_connection.load_extension(db_extension)
     except Exception as e:
-        raise RMPyExcep(e, "\n\n" "Cannot open the RM database file." "\n")
+        raise RM_Py_Exception(e, "\n\n" "Cannot open the RM database file." "\n")
     return db_connection
 
 
 # ===================================================DIV60==
-class RMPyExcep(Exception):
+class RM_Py_Exception(Exception):
 
     '''Exceptions thrown for configuration/database issues'''
 
