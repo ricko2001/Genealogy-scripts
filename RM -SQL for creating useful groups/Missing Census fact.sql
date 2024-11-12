4c4ad85afd6b232d39c5fdc1f4d151b1a23240d7
Richard Otter <RichardJOtter@gmail.com>
2024-11-12 11:34:52 -0800

    Search for missing census- moved year values to constants CTE

--- RM -SQL for creating useful groups/Missing Census fact.sql
+++ RM -SQL for creating useful groups/Missing Census fact.sql
@@ -1,19 +1,28 @@
-  -- Search for missing 1950 Census on appropriate people
-  -- people who might be in 1950 census but have no FACT or SHARED FACT
-  -- Requires Birth place in United States
-  -- Requires Birth birth date in specified range
-  -- Death Fact not required
-  -- Does not find people with Census fact or shared fact Detail 1950%
+  -- Search for missing Census on people with constraints:
+  -- people who might be in census but have no FACT or SHARED FACT
+  -- Requires Birth place in "United States"
+  -- Requires Birth birth date in specified range (between YearBirth and YearCensus)
+  -- Death date after YearCensus or no DeathFact
+  -- Person does not have a Census fact or shared census fact (Description starts with YearCensus)
   -- Does not find people not born in US but residing in USA in 1950
+  -- Does not support Census Family type event.
+  -- TO USE: EDIT LINES 14 TO 16
+  -- (make place '' for no selection of birth place e.g. if places do not include country)
   --
-  WITH existing_events AS
+  WITH
+  constants(C_YearCensus, C_YearBirth, C_PlaceBirth) AS (
+    SELECT   1930            AS C_YearCensus,
+             1830            AS C_YearBirth,
+             'United States' AS C_PlaceBirth
+  ),
+  existing_events AS  -- All census events whose Description starts with YearCensus
   (
   SELECT EventID
   FROM EventTable AS et
   INNER JOIN FactTypeTable AS ftt ON et.EventType = ftt.FactTypeID
   WHERE
       et.OwnerType = 0
-  AND et.Details LIKE ('1950%') COLLATE NOCASE
+  AND et.Details LIKE ( (SELECT C_YearCensus FROM constants) || '%') COLLATE NOCASE
   AND ftt.Name = 'Census' COLLATE NOCASE
   )
   --
@@ -25,19 +34,20 @@
     LEFT JOIN EventTable AS et_death  ON et_death.OwnerID = pt.PersonID
                                       AND et_death.EventType = 2
   WHERE
-    plt.Name LIKE '%United States%'
-    AND SUBSTR(et_birth.DATE, 4,4) < '1950'
-    AND SUBSTR(et_birth.DATE, 4,4) > '1850'
-    AND (SUBSTR(et_death.DATE, 4,4) > '1950' OR et_death.OwnerID IS NULL)
+    plt.Name LIKE '%' || (SELECT C_PlaceBirth FROM constants) || '%'
+    AND  CAST(SUBSTR(et_birth.DATE, 4,4) as Integer) < CAST((SELECT C_YearCensus FROM constants) as Integer)
+    AND  CAST(SUBSTR(et_birth.DATE, 4,4) as Integer) > CAST((SELECT C_YearBirth FROM constants) as Integer)
+    AND ( CAST(SUBSTR(et_death.DATE, 4,4) as Integer) > CAST((SELECT C_YearCensus FROM constants) as Integer)
+          OR et_death.OwnerID IS NULL)
   --
   EXCEPT
-  -- people who have a census fact of specified type
+  -- people who have a census fact of YearCensus type
   SELECT et.OwnerID
   FROM EventTable AS et
   INNER JOIN existing_events ON existing_events.EventID = et.EventID
   --
   EXCEPT
-  -- people who are witness to a census fact of specified type
+  -- people who are witness to a census fact of YearCensus type
   SELECT wt.PersonID
   FROM WitnessTable AS wt
   INNER JOIN existing_events ON existing_events.EventID = wt.EventID
